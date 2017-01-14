ad0d66cee30bf0dd2fdab406b30a2041348254ea
Gawain Lynch <gawain.lynch@gmail.com>
2017-01-14 19:50:18 +0100

    AbortException thrown when the execution is aborted with the SIGINT signal

---
+++ src/Exception/AbortException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Bolt\Installer\Exception;
+
+/**
+ * This exception is thrown when the execution is aborted with the SIGINT signal.
+ *
+ * @author Grégoire Pineau <lyrixx@lyrixx.info>
+ */
+class AbortException extends \RuntimeException
+{
+}
